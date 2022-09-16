12fa533ca8902fa29a55dc8d0ae6896827ab02fb
UnitedPuggs <edpoulsonv@hotmail.com>
2022-09-16 05:37:14 +0000

    Added new pages, created link to database

---
+++ html/buy_page.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buy Page</title>
+</head>
+
+<style>  
+ 
+.resize {  
+    width: 100px;
+    height: auto;  
+}  
+</style>
+
+<!-- Header Menu of the Page -->
+<header>
+         
+    <!-- Top header menu containing
+        logo and Navigation bar --> 
+        <!-- Navigation Menu -->
+        <nav>
+            <ul>
+            <div id="logo">
+                <img class = resize src="CarGos Logo.png" />
+            </div>
+
+            <div id="Profile">
+                <div> 
+                <a href="buy_page.php">  
+                    <button type="button" class="profile_button" >
+                        <img class = button-container src="Profile.png"/>
+                    </button> 
+                </a>
+                </div>
+            </div>
+                            
+            </ul>
+        </nav>  
+</header>
+
+<body>
+    <h1>
+        Buy Page
+    </h1>
+</body>
+</html>
