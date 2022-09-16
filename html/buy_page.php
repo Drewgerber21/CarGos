43886472e97edae64d041d75b36d7a866d622383
UnitedPuggs <edpoulsonv@hotmail.com>
2022-09-16 05:37:42 +0000

    Wrote mockup code for making and grabbing listings

--- html/buy_page.php
+++ html/buy_page.php
@@ -7,6 +7,21 @@
     <title>Buy Page</title>
 </head>
 
+<?php
+    $servername = "localhost";
+    $user = "root";
+    $password = "piWJbQv5Ksd8Yk";
+    $dbname = "CarGos";
+
+    $conn = mysqli_connect($servername, $user, $password, $dbname);
+    
+    if(!$conn) {
+        die("Connection failed " . mysqli_connect_error());
+    }
+
+    
+?>
+
 <style>  
  
 .resize {  
@@ -42,8 +57,40 @@
 </header>
 
 <body>
-    <h1>
-        Buy Page
-    </h1>
+    <a href="https://cargos.me">
+        <button class="back-button">Go Back</button>
+    </a>
+    <h1>Buy Page</h1>   
+    <div class="buyMainDiv">
+        <div class="buyButtonArray">
+            
+        </div>
+
+        <!-- Mockup for grabbing listing data from db -->
+        <div class="buyTempDiv">
+            <?php
+                $selectListing = "SELECT * FROM ListingInfo";
+                $result = mysqli_query($conn, $selectListing);
+                if(mysqli_num_rows($result) > 0) {
+                    echo "<table class=\"listingTable\">
+                    <tr><th>Listing Price</th>
+                    <th>Listing Description</th>
+                    <th>Listing Date</th>";
+                }
+
+                while($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr>
+                    <td>" . $row["ListingPrice"] . "</td>" .
+                    "<td>" . $row["ListingDesc"] . "</td>" .
+                    "<td>" . $row["ListingDate"] . "</td>"
+                    . "</tr>";
+                }
+                echo "</table>";
+            ?>
+        </div>
+    </div>
+    <?php
+        $conn->close();
+    ?>
 </body>
 </html>
--- html/sell_page.php
+++ html/sell_page.php
@@ -7,8 +7,50 @@
     <title>Sell Page</title>
 </head>
 <body>
-    <h1>
-        Sell Page
-    </h1>
+    <a href="https://cargos.me">
+        <button class="back-button">Go Back</button>
+    </a>
+
+<?php
+    $servername = "localhost";
+    $user = "root";
+    $password = "piWJbQv5Ksd8Yk";
+    $dbname = "CarGos";
+
+    $conn = mysqli_connect($servername, $user, $password, $dbname);
+    
+    if(!$conn) {
+        die("Connection failed " . mysqli_connect_error());
+    }
+
+    
+?>
+<h1>Sell Page</h1>
+    <div class="sellMainDiv">
+        <form method="post" action="sell_page.php" class="sellPageForm">
+        <!-- Creating a create listing mockup-->
+        <div class="sellButtonArray">
+            <label for="listingPrice" class="listingLabel price">Listing Price: $</label>
+            <input type="number" name="listingPrice" id="listingPrice" min="1" max="99999999" step="1" placeholder="0" required><br>
+            <label for="listingDesc" class="listingLabel desc">Listing Description: </label><br>
+            <input type="text" name="listingDesc" id="listingDesc"><br>
+            <input type="submit" value="Create Listing">
+            <?php
+                $listingID = null;
+                $userID = 2;
+                $listingPrice = $_POST["listingPrice"];
+                $listingDesc = $_POST["listingDesc"];
+                $listingDate = date("Y-m-d");
+
+                $insertListing = $conn->prepare("INSERT INTO ListingInfo(ListingID, UserID, ListingPrice, ListingDesc, ListingDate) VALUES(?, ?, ?, ?, ?);");
+                $insertListing->bind_param("iiiss", $listingID, $userID, $listingPrice, $listingDesc, $listingDate);
+                $insertListing->execute();
+            ?>
+        </div>
+        </form>
+    </div>
+    <?php
+        $conn->close();
+    ?>
 </body>
 </html>
